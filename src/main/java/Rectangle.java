9676d03d579513253cb2539f06015409f38593ef
Nathan Parmar <nathparmar@icloud.com>
2023-10-10 16:34:23 +0100

    Completed rectangle task

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,8 +1,33 @@
 public class Rectangle {
 
     private int length;
-    private int length;
-    private int length;
+    private int width;
+
+    private int area;
+
+
+    public Rectangle(int length, int width){
+        this.length = length;
+        this.width = width;
+
+    }
+//methods
+    public int getArea(){
+        return length*width;
+    }
+
+//    Getters
+
+     public int getLength(){
+        return this.length;
+
+     }public int getWidth(){
+        return this.width;
+     }
+
+     public boolean getSquare(){
+        return (length == width);
+     }
 
 
 }
